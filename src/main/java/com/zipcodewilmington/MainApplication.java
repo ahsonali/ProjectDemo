bf5b343a82af2ac71349105745691bb66487031c
Ahson Chaudhary <ahsonchaudhary@zipcoders-MacBook-Pro-82.local>
2018-02-06 15:46:38 -0500

    This is first commit

---
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington;
+
+public class MainApplication {
+
+    //Main Method
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
